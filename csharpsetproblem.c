0b4cec671ea5d0f7cbb139f397dd8b41dd21b843
realityfabric <realityfabric@gmail.com>
2018-09-30 13:06:57 -0700

    Move modified C# tests to its own main.c

--- csharpsetproblem.c
+++ csharpsetproblem.c
@@ -18,6 +18,45 @@
 #include <stdio.h>
 #include "csharpsetproblem.h"
 
+int main () {
+	boolean a[100], b[100], c[100];
+
+	initializeSet(a);
+	initializeSet(b);
+	initializeSet(c);
+
+	insertValue(a, 1);
+	insertValue(a, 2);
+	insertValue(a, 3);
+	insertValue(a, 4);
+	insertValue(a, 5);
+
+	insertValue(b, 2);
+	insertValue(b, 4);
+	insertValue(b, 6);
+	insertValue(b, 8);
+	insertValue(b, 10);
+
+	printf("Printing set A:\n");
+	printSet(a);
+	printf("Printing set B:\n");
+	printSet(b);
+
+	findUnionSet(a, b, c);
+	printf("Printing set C (union set):\n");
+	printSet(c);
+
+	deleteValue(a, 4);
+
+	findUnionSet(a, b, c);
+	printf("Printing set A after deleting value 4:\n");
+	printSet(a);
+	printf("Printing set C (union set):\n");
+	printSet(c);
+
+	return 0;
+}
+
 // initialize a boolean array with 100 elements so that each element = false
 boolean* initializeSet (boolean* arr) {
 	for (int i = 0; i < 100; i++) {
--- main.c
+++ main.c
@@ -2,40 +2,5 @@
 #include "csharpsetproblem.h"
 
 int main () {
-	boolean a[100], b[100], c[100];
-
-	initializeSet(a);
-	initializeSet(b);
-	initializeSet(c);
-
-	insertValue(a, 1);
-	insertValue(a, 2);
-	insertValue(a, 3);
-	insertValue(a, 4);
-	insertValue(a, 5);
-
-	insertValue(b, 2);
-	insertValue(b, 4);
-	insertValue(b, 6);
-	insertValue(b, 8);
-	insertValue(b, 10);
-
-	printf("Printing set A:\n");
-	printSet(a);
-	printf("Printing set B:\n");
-	printSet(b);
-
-	findUnionSet(a, b, c);
-	printf("Printing set C (union set):\n");
-	printSet(c);
-
-	deleteValue(a, 4);
-
-	findUnionSet(a, b, c);
-	printf("Printing set A after deleting value 4:\n");
-	printSet(a);
-	printf("Printing set C (union set):\n");
-	printSet(c);
-
 	return 0;
 }
