f62216546291c5367741e5a54c1392357548907c
realityfabric <realityfabric@gmail.com>
2019-05-14 18:56:46 -0700

    Add chapter 2 problem 4

---
+++ starting-out-with-java-4th-edition/Chapter-02/03-Personal-Information/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+	char* name = "RealityFabric";
+	char* address = "555 Sunny Street, Pleasantville, OH 55555";
+	char* phone = "1-800-555-5555";
+	char* major = "Information Technology";
+
+	// use only one printf statement
+	printf("Name: %s\nAddress: %s\nPhone Number: %s\nMajor: %s\n", name, address, phone, major);
+
+	return 0;
+}
