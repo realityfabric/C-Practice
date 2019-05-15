4d54fe69ae2f1749f2fb9ee0a4f234f55a4cc240
realityfabric <realityfabric@gmail.com>
2019-05-14 18:42:00 -0700

    Add Chapter 2, Problem 1

---
+++ starting-out-with-java-4th-edition/Chapter-02/01-Name-Age-and-Annual-Income/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+	char* name = "RealityFabric";
+	int age = 10; // :)
+	double annualPay = 80000;
+
+	printf("My name is %s, my age is %d, and I hope to earn $%f per year.\n", name, age, annualPay);
+
+	return 0;
+}
