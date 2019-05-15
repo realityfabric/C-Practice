70353b3f9f0c4c74eb7ee99772367df520d39645
realityfabric <realityfabric@gmail.com>
2019-05-14 18:59:44 -0700

    Demonstrate sums

---
+++ starting-out-with-java-4th-edition/Chapter-02/05-Sum-of-Two-Numbers/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+	int a = 62,
+	    b = 99,
+	    sum;
+
+	sum = a + b;
+
+	// the problem said to store the variable
+	// that's it. Store 62, store 99, and store the sum in another variable.
+	// :)
+
+	return 0;
+}
